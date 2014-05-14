5e60eba7668a36031ed8e5e3c4490ac294b87849
Antariksh Bothale <abothale@uw.edu>
2014-05-14 15:32:54 -0700

    Added processing for HUM and NUM:date types

--- src/AnswerProcessingTaskExecutor.py
+++ src/AnswerProcessingTaskExecutor.py
@@ -5,18 +5,19 @@
 # Comes up with an answer given the relevant passages and query 
 
 from TaskExecutor import *
-import pickle, os, sys
+import pickle, os, sys, nltk
 
 class AnswerProcessingTaskExecutor(TaskExecutor):
     def __init__(self):
         TaskExecutor.__init__(self, "AnswerProcessingTaskExecutor")
+    
     
     def Execute(self, session):
 
         session.answers = []
         # Just trying out if putting an answer containing date / time for a WHEN query is sensible
         # and improves scoring. Crudely done. This is heavily hardcoded.
-
+        
         monthRegex = r'(january|february|march|april|may|june|july|august|september|october|november|december)'
         yearRegex = r'\d{4}'
         daySet = set(['sunday','monday','tuesday','wednesday','thursday','friday','saturday'])
@@ -25,20 +26,35 @@ class AnswerProcessingTaskExecutor(TaskExecutor):
         badAnswers = []
 
         answerType = session.answerType
-
+        
         for passage, docId in session.relevantPassages[:20]:
             passage = passage.replace("\n", " ")
 
-
-            if answerType == "NUM:date":
-            #if session.question.category in ["DATETIME", "DATE", "DAY", "MONTH", "YEAR"]:
-                print ("When Question Found:" + session.question.text)
+            ne_tree = nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(passage)))
+            ne_types = []
+            for item in ne_tree:
+                try:
+                    ne_types.append(str(item.node))
+                except AttributeError:
+                    pass
+            
+            if answerType[:3] == "HUM" and "PERSON" in ne_types:
+                print ("Answer is of HUM type!")
+                # If answer is of some human type                
+                goodAnswers.append((passage, docId))
+            elif answerType == "NUM:date":
+                print ("When Question Found!")
                 if re.match(monthRegex, session.question.text.lower()) or len(daySet.intersection(session.question.text.lower().split())) > 0 or re.match(yearRegex, session.question.text.lower()):
                     goodAnswers.append((passage, docId))
-                else:
+                else:    
                     badAnswers.append((passage, docId))
             else:
-                goodAnswers.append((passage, docId))
+                badAnswers.append((passage, docId))
+        
+                #goodAnswers.append((passage, docId))
+            #if session.question.category in ["DATETIME", "DATE", "DAY", "MONTH", "YEAR"]:
+                
+
 
         session.answers = goodAnswers + badAnswers
         for (passage, docId) in session.answers:
