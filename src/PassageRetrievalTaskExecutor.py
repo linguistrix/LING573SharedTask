298851c9ec22a8c8b9a87afba3c5ecc46a73dd88
Julian Chan <masterchan42@gmail.com>
2014-05-22 14:57:24 -0700

    Fixed sort inconsistency in PassageRetrievalTaskExecutor.py

--- src/PassageRetrievalTaskExecutor.py
+++ src/PassageRetrievalTaskExecutor.py
@@ -27,6 +27,7 @@ class PassageRetrievalTaskExecutor(TaskExecutor):
         results = session.relevantDocuments
 
         qwords = frozenset(term[1] for term in q.all_terms())
+        session.logs.append("Passage retrieval qwords: {0}".format(",".join(qwords)))
 
         for result in results:
             id = result[0]
@@ -38,14 +39,21 @@ class PassageRetrievalTaskExecutor(TaskExecutor):
             tokens = self.analyzer(body, positions=True, chars=True, removestops=False)
             tokens = whoosh.highlight.set_matched_filter(tokens, qwords)
 
-            triples.extend( (self.scorer(f), f, id) for f in self.fragmenter.fragment_tokens(body, tokens) )
+            triples.extend(
+                (self.scorer(f),
+                 removeNewline(self.formatter.format_fragment(f)),
+                 id)
+                    for f in self.fragmenter.fragment_tokens(body, tokens) )
         
         triples.sort(reverse=True)
 
         relevantPassages = []
         for triple in triples:
-            passage = self.formatter.format_fragment(triple[1])
-            relevantPassages.append( (removeNewline(passage), triple[2]) )
+            score = triple[0]
+            relevantPassage = triple[1] 
+            id = triple[2]
+            relevantPassages.append((relevantPassage, id))
+            session.logs.append("Relevant passage: {0} | score: {1}".format(relevantPassage, score)) 
        
         session.relevantPassages = relevantPassages
         
