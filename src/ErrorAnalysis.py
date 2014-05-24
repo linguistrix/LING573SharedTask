5192aafabb7f5ee4c9dfab278ce54c14b69ba201
Antariksh Bothale <antariksh.bothale@gmail.com>
2014-05-23 18:04:59 -0700

    Working on ErrorAnalysis

--- src/ErrorAnalysis.py
+++ src/ErrorAnalysis.py
@@ -5,6 +5,17 @@
 # Module that takes in a question file, factoids file, output file and results file
 # and gives out Error Analysis Statistics
 import sys, os
+from bs4 import BeautifulSoup
+from SystemEvaluator import GetAllQuestions
 
 if __name__ == '__main__':
-    pass
+    questionsFile = sys.argv[1]
+    #factoidsFile = sys.argv[2]
+    #outputFile = sys.argv[3]
+    #resultsFile = sys.argv[4]
+
+    questions = GetAllQuestions(questionsFile)
+
+    for q in questions:
+        print q
+
